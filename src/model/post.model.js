8e4ec64806292441288f4865f11b0bc8d51ebfe5
djoetker <jansiede@posteo.de>
2024-04-09 10:03:39 +0200

    update error handling

--- src/model/post.model.js
+++ src/model/post.model.js
@@ -22,60 +22,85 @@ postSchema.index({ content: 'text' });
 export default Post;
 
 export async function createPost(post) {
-  const newEntry = new Post(post);
-  const entry = await newEntry.save();
-  return entry;
+  try {
+    const newEntry = new Post(post);
+    const entry = await newEntry.save();
+    return entry;
+  } catch (error) {
+    console.error("Error creating post:", error);
+    throw error;
+  }
 };
 
 export async function addCommentsToPostById(postId, commentId) {
-  const updatedPost = await Post.findByIdAndUpdate(
-    { _id: postId },
-    { $push: { comments: commentId } },
-    { new: true }
-  );
-  return updatedPost;
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      { _id: postId },
+      { $push: { comments: commentId } },
+      { new: true }
+    );
+    return updatedPost;
+  } catch (error) {
+    console.error("Error adding comment:", error);
+    throw error;
+  }
 };
 
 export async function findPostById(postId) {
-  const post = await Post.findById(postId).populate("comments");
-  return post;
+  try {
+    const post = await Post.findById(postId).populate("comments");
+    return post;
+  } catch (error) {
+    console.error("Error finding post by ID:", error);
+    throw error;
+  }
 };
 
 
 export async function findRandomPosts(previousPostIds = []) {
-  const prevIds = previousPostIds.map((id) => new ObjectId(id));
-  const posts = await Post.aggregate([
-    { $match: { _id: { $nin: prevIds } } },
-    { $sample: { size: 8 } }
-  ]);
-  const fetchedPostIds = posts.map(post => post._id);
-  const updatedPostIds = [...previousPostIds, ...fetchedPostIds];
-  return { posts, updatedPostIds };
+  try {
+    const prevIds = previousPostIds.map((id) => new ObjectId(id));
+    const posts = await Post.aggregate([
+      { $match: { _id: { $nin: prevIds } } },
+      { $sample: { size: 8 } }
+    ]);
+    const fetchedPostIds = posts.map(post => post._id);
+    const updatedPostIds = [...previousPostIds, ...fetchedPostIds];
+    return { posts, updatedPostIds };
+  } catch (error) {
+    console.error("Error finding random posts:", error);
+    throw error;
+  }
 };
 
 export async function updatePosts(previousPostIds = []) {
-  const prevIds = previousPostIds.map((id) => new ObjectId(id));
-  const response = await Post.aggregate([
-    {
-      $match: {
-        _id: {
-          $in: prevIds
+  try {
+    const prevIds = previousPostIds.map((id) => new ObjectId(id));
+    const response = await Post.aggregate([
+      {
+        $match: {
+          _id: {
+            $in: prevIds
+          }
         }
-      }
-    },
-    {
-      $facet: {
-        order: [
-          { $match: { _id: { $in: prevIds } } },
-          { $addFields: { __order: { $indexOfArray: [prevIds, "$_id"] } } },
-          { $sort: { __order: 1 } }
-        ]
-      }
-    },
-    { $unwind: "$order" },
-    { $replaceRoot: { newRoot: "$order" } }
-  ]);
-  return response;
+      },
+      {
+        $facet: {
+          order: [
+            { $match: { _id: { $in: prevIds } } },
+            { $addFields: { __order: { $indexOfArray: [prevIds, "$_id"] } } },
+            { $sort: { __order: 1 } }
+          ]
+        }
+      },
+      { $unwind: "$order" },
+      { $replaceRoot: { newRoot: "$order" } }
+    ]);
+    return response;
+  } catch (error) {
+    console.error("Error updating posts:", error);
+    throw error;
+  };
 };
 
 export async function searchPosts(previousPostIds = [], searchQuery) {
